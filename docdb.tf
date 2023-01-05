16e76e1bc055f990d545489684d3e759ff1987cc
Divya <divya5guntaka@gmail.com>
2023-01-05 20:02:09 +0530

    Update

--- docdb.tf
+++ docdb.tf
@@ -15,6 +15,10 @@ resource "aws_docdb_cluster" "docdb" {
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
   skip_final_snapshot     = var.skip_final_snapshot
+  kms_key_id              = var.kms_key_id
+  vpc_security_group_ids  = [aws_security_group.allow_docdb.id]
+  db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
+  storage_encrypted       = var.storage_encrypted
 }
 
 resource "aws_docdb_cluster_instance" "instances" {
